80d2af882b1e81d4f2ba2e6d00910c883a0d2e6e
Fabricio <fabricioj49@gmail.com>
2021-11-24 18:25:07 -0300

    app

--- app/src/main/java/br/com/fabreba/projetoresistores/MainActivity.java
+++ app/src/main/java/br/com/fabreba/projetoresistores/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity{
             String numero =  txtname.getText().toString();
             int number = Integer.parseInt(numero);
             int[] checker = {99,999,9999,99999,999999,9999999,99999999};
-            for (int i=0;i< checker.length;i++){
+            for (int i=0;i < checker.length;i++){
                 if(number==checker[i]){
                     number++;
                     numero = ""+number;
@@ -62,16 +62,24 @@ public class MainActivity extends AppCompatActivity{
                 }
             }
 
+
             String mil = numero.substring(0,1);
+
             String cent = numero.substring(1,2);
-            String dez = numero.substring(2,3);
+
+
             System.out.println(mil);
             int faixa1 =  Integer.parseInt(mil);
             int faixa2 = Integer.parseInt(cent);
-            int faixa3 = Integer.parseInt(dez);
+
             int tamanho = numero.length();
 
             tamanho = tamanho-2;
+            if(tamanho<0){
+                tamanho = 0;
+            }
+            int faixa3 = tamanho;
+            System.out.println("Tamanho do resistor"+tamanho);
             String[] a = {"#000000","#654321","#FF0000","#FF7F00","#FFFF00","#008000","#0000FF","#A020F0","#808080","#FFFAFA"};
             //                      Marrom,Vermelho,"Laranja","Amarelo","Verde","Azul" ,"Magenta","Cinza" e "Branco"
             String[] multi = {"#000000","#654321","#FF0000","#FFA500","#FFFF00","#008000","#0000FF","#A020F0","#808080"};
@@ -113,17 +121,18 @@ public class MainActivity extends AppCompatActivity{
             int div = 1;
             String unit = unidade[0];
             String searchUnit = unidadeSearch[0];
-            if(number>1&&number<1000){
+
+            if(number>=1&&number<1000){
+                div=divUnit[0];
+                unit=unidade[0];
+                searchUnit = unidadeSearch[0];
+            }
+            else if(number>=1000&&number<1000000){
                 div=divUnit[1];
                 unit=unidade[1];
                 searchUnit = unidadeSearch[1];
             }
-            else if(number>1000&&number<1000000){
-                div=divUnit[1];
-                unit=unidade[1];
-                searchUnit = unidadeSearch[1];
-            }
-            else if(number>1000000&&number<1000000000){
+            else if(number>=1000000&&number<1000000000){
                 div=divUnit[2];
                 unit=unidade[2];
                 searchUnit = unidadeSearch[2];
@@ -152,7 +161,7 @@ public class MainActivity extends AppCompatActivity{
             Toast.makeText(this, "Clique no botão para pesquisar", Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
-
+            System.out.println(e);
 
             Toast toast = Toast.makeText(this,"Escreva uma tolerância\n ou uma resistência",Toast.LENGTH_SHORT);
             toast.show();
